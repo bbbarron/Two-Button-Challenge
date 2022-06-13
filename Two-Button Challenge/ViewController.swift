44279ff7a237094d9de02bf50d1cda59075f7256
Barry Barron <barrybbarron@gmail.com>
2022-06-13 10:38:32 -0700

    This is the initial commit of the 2-Button challenge. No colors etc have been added. This completed chapter 2.3

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var selectButtonBelow: UILabel!
+    
+    @IBAction func button1Pressed(_ sender: UIButton) {
+        selectButtonBelow.text = "Left Button Pressed"
+    }
+    
+    @IBAction func button2Pressed(_ sender: UIButton) {
+        selectButtonBelow.text = "Right Button Pressed"
+    }
 }
 
